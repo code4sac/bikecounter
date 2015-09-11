776f9bea616b256c179351acb38b52d0be557c4f
Jason Ody <jasonody@users.noreply.github.com>
2015-09-10 19:24:32 -0700

    Converted to using angular
    - removed backbone and marionette specific files and libraries
    - removed jQuery
    - added angular library
    - created an angular module for the bike counter app

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-// This app is built on the backbone and marionette frameworks,
-// It tries to use them as simply as possible.
-var Marionette = require('backbone.marionette');
+var angular = require('angular');
 
-var app = new Marionette.Application();
-
-module.exports = app;
+angular.module('bikeCounter', []);
