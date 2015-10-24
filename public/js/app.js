cb219fcf62e1dfcfce0b619a4f2d605eb069d4ca
Jason Ody <jasonody@users.noreply.github.com>
2015-10-23 19:41:39 -0700

    Added retrieve data for a count
    - Count ID is retrieved from query string
    - GET request to 'counts' route to retrieve data for count

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
-var $ = require('jquery');
-
 $(document).ready(function () {
 	
+	var countId = $.QueryString["c"];
+	var url = 'counts/' + countId;
 	
+	$.get(url)
+		.then(function (data) {
+			
+			console.log(data);
+			startUp(data);
+		});
 });
---
+++ public/js/mixins.js
@@ -0,0 +1,15 @@
+(function() {
+	
+	//http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+	$.QueryString = (function(a) {
+		if (a == "") return {};
+		var b = {};
+		for (var i = 0; i < a.length; ++i)
+		{
+			var p=a[i].split('=');
+			if (p.length != 2) continue;
+			b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+		}
+		return b;
+	})(window.location.search.substr(1).split('&'))
+})();
